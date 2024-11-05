1f0a44ce130ae076821a33c9d16f6d2d671771be
alberto_clase <asanabriagarcia@safareyes.es>
2024-11-05 16:40:12 +0100

    Creación del proyecto

---
+++ src/main/java/com/example/ecoloco/EcolocoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecoloco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcolocoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcolocoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecoloco/EcolocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecoloco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcolocoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
